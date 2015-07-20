c51720da11f00ea418fee1638cd3a5f82f81e1ab
lxb <leixb@agilean.cn>
2015-07-20 23:55:42 +0800

    pom add version number filter。 install add index.html as default file.

--- src/main/webapp/install.js
+++ src/main/webapp/install.js
@@ -3,29 +3,37 @@
  */
 
 var root = "";
+var pathname = document.location.pathname;
+if (/\/$/.test(pathname)) {
+    pathname = pathname + "index.html";
+}
+
 var delta = 0;
-if (document.location.pathname.indexOf(root) == 0) {
-  delta = 2;
+if (pathname.indexOf(root) == 0) {
+    delta = 2;
 }
 var relativeMe = ""
-for (var i = 0; i < document.location.pathname.split("/").length - 2 - delta; i++) {
-  relativeMe = relativeMe + "../";
+for (var i = 0; i < pathname.split("/").length - 2 - delta; i++) {
+    relativeMe = relativeMe + "../";
 }
 
 function js(url) {
-  document.write('<s' + 'cript type="text/javascript" src="' + relativeMe + url.substr(1) + '"></' + 'script>')
+    document.write('<s' + 'cript type="text/javascript" src="' + relativeMe
+            + url.substr(1) + '"></' + 'script>')
 }
 
 function css(url) {
-  document.write('<link rel="stylesheet" href="' + relativeMe + url.substr(1) + '" />')
+    document.write('<link rel="stylesheet" href="' + relativeMe + url.substr(1)
+            + '" />')
 }
-app = document.location.pathname.split("/").pop().replace(/.html?$/, '');
-//meta
-//document.write('<meta http-equiv="Content-Security-Policy" content="default-src \'self\' http://182.92.7.14/ ;script-src \'self\' \'unsafe-inline\' \'unsafe-eval\'">')
-
+app = pathname.split("/").pop().replace(/.html?$/, '');
+// meta
+// document.write('<meta http-equiv="Content-Security-Policy"
+// content="default-src \'self\' http://182.92.7.14/ ;script-src \'self\'
+// \'unsafe-inline\' \'unsafe-eval\'">')
 
 // for all apps.
-//js("/node_modules/closure-library/closure/goog/base.js");
+// js("/node_modules/closure-library/closure/goog/base.js");
 
 js("/assets/jquery-1.9.1.js");
 js("/assets/js/bootstrap.js")
@@ -36,4 +44,4 @@ css("/assets/orion/built-editor.css")
 js("/assets/orion/built-editor.js")
 
 // for controller js.
-js("/"+document.location.pathname.split('/').pop().replace(/.html?$/, "_.js"));
+js("/" + pathname.split('/').pop().replace(/.html?$/, "_.js"));
